3229d74fb33457efa8292863d9644883f65bd7fa
Jessica Bryant <jessica.bryant@live.com>
2021-10-12 10:44:27 -0400

    Fix errors and add notes to LoopsExercise

--- script.js
+++ script.js
@@ -24,12 +24,11 @@ for (wizard of wizards) {
 // 4a
 let harryPotterMovies = 0;
 // 4b
-while (harryPotterMovies <= 8) {
-    console.log(harryPotterMovies);
+while (harryPotterMovies < 8) {
     harryPotterMovies++;
 } 
-// 4c I do not think this is right, but I can't figure out another way. I tried to do a break in the above while loop, but I got into an infinite loop somehow and had to kill terminal.
-console.log(harryPotterMovies-1);
+// 4c This is now correct. I had to change the <= 8 to < 8 and take out the console.log from the while loop.
+console.log(harryPotterMovies);
 
 
 // 5a
@@ -54,4 +53,29 @@ const quote = [
     "Harry"
 ];
 // 6b I know this isn't a loop, but it gets the result.
-console.log(quote.join(" "));
+// console.log(quote.join(" "));
+// Maianne's way with the loop:
+// let sentence = "";
+// while (quote !== sentence) {
+//     console.log(quote.join(" "));
+//     break;
+// }
+// Matt's way with the FOR OF loop:
+let sentence = "";
+for (word of quote) {
+    sentence += word + " ";
+    // OR
+    // sentence += `${word} `; This is better because the template literal allows us to add the space right after each word.
+}
+// we need to use trim to remove the extra space from the end
+// sentence = sentence.trim();
+// console.log(sentence);
+// OR
+console.log(sentence.trim());
+
+// Matt said we could also do it this way USING A FOR LOOP:
+// let sentence = "";
+// for (i = 0; i < quote.length; i++) {
+    // sentence += `${quote[i]} `;
+// }
+// console.log(sentence.trim());
